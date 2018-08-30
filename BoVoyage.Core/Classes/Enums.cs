468eface0527486fb1b756f85e5e331f82461640
fredturiot <f.turiot@gmail.com>
2018-08-30 12:45:35 +0200

    Ajout des  3 Enumerations

---
+++ BoVoyage.Core/Classes/Enums.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoVoyage.Core.Classes
+{
+    public class Enums
+    {
+        enum TypeAssurance { Annulation = 1 };
+        enum EtatDossierResevation { enAttente, enCours, refusee, acceptee };
+        enum RaisonAnnulationDossier { client = 1, placesInsuffisantes };
+
+
+    }
+}
