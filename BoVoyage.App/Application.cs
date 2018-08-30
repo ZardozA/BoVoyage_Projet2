a5be5c7e95ca711a1a14fe1fe0022f1d7e18e962
AlexinaO <alexina.orsoni@gmail.com>
2018-08-31 01:11:48 +0200

    Suppression des commentaires sur la ligne ModuleClients

--- BoVoyage.App/Application.cs
+++ BoVoyage.App/Application.cs
@@ -9,14 +9,16 @@ namespace BoVoyage.App
             : base("Bo Voyage - La Super Agence de Voyages moins chers !")
         {
         }
-        //public ModuleClients ModuleClients { get; private set; }
+        public ModuleClients ModuleClients { get; private set; }
+        //public ModuleParticipants ModuleParticipants { get; private set; }
         //public ModuleVoyages ModuleVoyages { get; private set; }
         public ModuleAgences ModuleAgences { get; private set; }
         //public ModuleDossiers ModuleDossiers { get; private set; }
 
         protected override void InitialiserModules()
         {
-            //this.MdduleClients = this.AjouterModule(new ModuleClients(this, "Gestion des Clients et des Participants"));
+            this.ModuleClients = this.AjouterModule(new ModuleClients(this, "Gestion des Clients"));
+            //this.MdduleParticipants = this.AjouterModule(new ModuleClients(this, "Gestion des Participants"));
             //this.MdduleVoyages = this.AjouterModule(new ModuleVoyages(this, "Gestion des Voyages"));
             this.ModuleAgences = this.AjouterModule(new ModuleAgences(this, "Gestion des Agences"));
             //this.ModuleDossiers = this.AjouterModule(new ModuleDossiers(this, "Gestion des Dossiers de Réservation"));
