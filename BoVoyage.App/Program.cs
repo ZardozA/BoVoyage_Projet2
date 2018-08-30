c5ef3af14ee5471b9079647c63e0545fba9e4140
AlexinaO <alexina.orsoni@gmail.com>
2018-08-30 14:40:36 +0200

    Ajout du namespace BoVoyage.Core dans Program.cs

--- BoVoyage.App/Program.cs
+++ BoVoyage.App/Program.cs
@@ -3,13 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BoVoyage.Core;
 
-namespace BoVoyage_Projet2
+namespace BoVoyage.App
 {
     class Program
     {
         static void Main(string[] args)
         {
+            var application = new Application();
+            application.Demarrer();
         }
     }
 }
