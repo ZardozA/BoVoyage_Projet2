fe4387e3b8842c7e307a82d0fc181eb50d139b1d
AlexinaO <alexina.orsoni@gmail.com>
2018-08-31 11:07:33 +0200

    Déblocage de tous les modules dans Application

--- BoVoyage.App/Application.cs
+++ BoVoyage.App/Application.cs
@@ -10,18 +10,18 @@ namespace BoVoyage.App
         {
         }
         public ModuleClients ModuleClients { get; private set; }
-        //public ModuleParticipants ModuleParticipants { get; private set; }
+        public ModuleParticipants ModuleParticipants { get; private set; }
         public ModuleVoyages ModuleVoyages { get; private set; }
         public ModuleAgences ModuleAgences { get; private set; }
-        //public ModuleDossiers ModuleDossiers { get; private set; }
+        public ModuleDossiers ModuleDossiers { get; private set; }
 
         protected override void InitialiserModules()
         {
             this.ModuleClients = this.AjouterModule(new ModuleClients(this, "Gestion des Clients"));
-            //this.ModuleParticipants = this.AjouterModule(new ModuleClients(this, "Gestion des Participants"));
+            this.ModuleParticipants = this.AjouterModule(new ModuleParticipants(this, "Gestion des Participants"));
             this.ModuleVoyages = this.AjouterModule(new ModuleVoyages(this, "Gestion des Voyages"));
             this.ModuleAgences = this.AjouterModule(new ModuleAgences(this, "Gestion des Agences"));
-            //this.ModuleDossiers = this.AjouterModule(new ModuleDossiers(this, "Gestion des Dossiers de Réservation"));
+             this.ModuleDossiers = this.AjouterModule(new ModuleDossiers(this, "Gestion des Dossiers de Réservation"));
 
 
         }
