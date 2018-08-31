3924b66d4ae05fb5814978d549d4de8d7be99195
AlexinaO <alexina.orsoni@gmail.com>
2018-08-31 11:25:09 +0200

    Ajout d'un mapping sur id

--- BoVoyage.Core/Classes/Client.cs
+++ BoVoyage.Core/Classes/Client.cs
@@ -1,9 +1,11 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace BoVoyage.Core
 {
     public class Client : Personne
     {
+        [Column("IdClient")]
         public int Id { get; set; }
 
         public string Email { get; set; }
