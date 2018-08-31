0e72cee3f26a8bd7de6aa8fbd90570125cd7570a
AlexinaO <alexina.orsoni@gmail.com>
2018-08-31 11:25:21 +0200

    Ajout d'un mapping sur id

--- BoVoyage.Core/Classes/Participant.cs
+++ BoVoyage.Core/Classes/Participant.cs
@@ -1,7 +1,10 @@
-﻿namespace BoVoyage.Core
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BoVoyage.Core
 {
     public class Participant :Personne
     {
+        [Column("IdParticipant")]
         public int Id { get; set; }
 
         public double? Reduction { get; set; }
